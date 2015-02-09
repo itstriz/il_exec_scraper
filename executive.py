6c8bb5a471a72909027b3de3f485e0db0f383ed8
Matt Johnson <mjohn23@gmail.com>
2015-02-09 17:12:14 -0600

    Save to file

--- executive.py
+++ executive.py
@@ -10,11 +10,11 @@ soup = BeautifulSoup(html)
 
 all_links = soup.findAll('a')
 hrefs = []
+p = re.compile('^\/Government\/ExecOrders\/Pages\/[0-9]{4}\_[0-9]{2}.aspx$')
 for link in all_links:
-    if link.has_attr('href'):
+    if link.has_attr('href') and p.match(link['href']):
         hrefs.append(link['href'])
 
-p = re.compile('^\/Government\/ExecOrders\/Pages\/[0-9]{4}\_[0-9]{2}.aspx$')
-for href in hrefs:
-    if p.match(href):
-        print href
+f = open('executive_orders.txt', 'r')
+print f.read()
+f.close()
